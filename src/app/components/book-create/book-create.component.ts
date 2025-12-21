f4da39013c7964d8b1778b39d9379fe182436269
dada1020-davidd <dada1020@h-ka.de>
2025-12-21 23:28:04 +0100

    feat: Basisstruktur für die Neuanlegen-Komponente implementiert

---
+++ src/app/components/book-create/book-create.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './book-create.html',
+  styles: []
+})
+export class BookCreateComponent {
+}
