993ea0d5ed1939c254a084f6e29e0a10e2c605cc
vabujl <coda1014@h-ka.de>
2025-12-22 12:10:00 +0100

    feat: Route für die Suchfunktion ergänzt

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Search } from './search/search';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'search', component: Search },
+  { path: '', redirectTo: 'search', pathMatch: 'full',},
+];
