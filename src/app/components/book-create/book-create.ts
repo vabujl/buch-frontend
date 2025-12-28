37cb3469916849e75ac6c8ca9c91a88df759cc00
dada1020-davidd <dada1020@h-ka.de>
2025-12-28 13:57:13 +0100

    feat: Buch Neuanlegen Logik implementiert und HTML angepasst

--- src/app/components/book-create/book-create.ts
+++ src/app/components/book-create/book-create.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './book-create.html',
-  styleUrl: './book-create.css'
+  styleUrl: './book-create.css',
 })
 export class Create {
+  private readonly router = inject(Router);
+
+  book = {
+    titel: '',
+    untertitel: '',
+    autor: '',
+    isbn: '',
+    preis: 0,
+    rabatt: 0,
+    buchart: '',
+    rating: 0,
+    homepage: '',
+    javascript: false,
+    typescript: false,
+    erscheinungsdatum: '',
+    lieferbar: false,
+  };
+
+  save() {
+    console.log('Buch gespeichert:', this.book);
+    void this.router.navigate(['/search']);
+  }
+
+  cancel() {
+    void this.router.navigate(['/search']);
+  }
 }
